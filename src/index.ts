cec6a8d5f128766e954ae31e5503cd53c621bf4e
August Herron <66635096+augustryder@users.noreply.github.com>
2024-05-20 23:00:08 -0500

    added start/pause button

--- src/index.ts
+++ src/index.ts
@@ -25,62 +25,81 @@ const pendulumContainer = new Container();
 pendulumContainer.position = new Point(origin.x, origin.y);
 app.stage.addChild(pendulumContainer);
 
-// Draw pendulum graphics
-var pos1 = new Point(pendulum1.position(0, 0).x, pendulum1.position(0, 0).y);
-var pos2 = new Point(pendulum2.position(pos1.x, pos1.y).x, pendulum2.position(pos1.x, pos1.y).y);
+const initPos1 = new Point(pendulum1.position(0, 0).x, pendulum1.position(0, 0).y);
+const initPos2 = new Point(pendulum2.position(initPos1.x, initPos1.y).x, pendulum2.position(initPos1.x, initPos1.y).y);
+var pos1 = initPos1;
+var pos2 = initPos2;
 
 const arm1 = new Graphics();
-arm1.lineStyle(2, 0x000000)
-	.lineTo(pos1.x, pos2.y);
-
 const bob1 = new Graphics();
-bob1.position = pos1;
-bob1.beginFill(0x000000)
-  	.drawCircle(0, 0, 10)
- 	.endFill();
-
 const arm2 = new Graphics();
-arm2.position = pos1;
-arm2.lineStyle(2, 0x000000)
-	.lineTo(pos1.x, pos1.y);
-
 const bob2 = new Graphics();
-bob2.position = pos2;
-bob2.beginFill(0x000000)
-	.drawCircle(0, 0, 10)
-	.endFill();
 
 pendulumContainer.addChild(arm1);
 pendulumContainer.addChild(bob1);
 pendulumContainer.addChild(arm2);
 pendulumContainer.addChild(bob2);
 
+// Draw pendulum graphics
+function drawSystem(p1: Point, p2: Point) {
+
+	arm1.lineStyle(2, 0x000000)
+		.lineTo(p1.x, p1.y);
+	
+	bob1.position = p1;
+	bob1.beginFill(0x000000)
+		.drawCircle(0, 0, 10)
+		.endFill();
+	
+	arm2.position = p1;
+	arm2.lineStyle(2, 0x000000)
+		.lineTo(p1.x, p1.y);
+	
+	bob2.position = p2;
+	bob2.beginFill(0x000000)
+		.drawCircle(0, 0, 10)
+		.endFill();
+}
+
+drawSystem(initPos1, initPos2);
+
+let paused = true;
+
 app.ticker.add(() => {
   // Leapfrog integration
-  pendulum1.angularVelocity += pendulum1.angularAcceleration * dt / 2;
-  pendulum2.angularVelocity += pendulum2.angularAcceleration * dt / 2;
-
-  pendulum1.angle += pendulum1.angularVelocity * dt;
-  pendulum2.angle += pendulum2.angularVelocity * dt;
-
-  pendulum1.updateAngularAcceleration(pendulum1, pendulum2, g);
-  pendulum2.updateAngularAcceleration(pendulum1, pendulum2, g);
-
-  pendulum1.angularVelocity += pendulum1.angularAcceleration * dt / 2;
-  pendulum2.angularVelocity += pendulum2.angularAcceleration * dt / 2;
-
-  // Update transformations
-  pos1.x = pendulum1.length * Math.sin(pendulum1.angle);
-  pos1.y = pendulum1.length * Math.cos(pendulum1.angle);
-
-  pos2.x = pos1.x + pendulum2.length * Math.sin(pendulum2.angle);
-  pos2.y = pos1.y + pendulum2.length * Math.cos(pendulum2.angle);
-
-  arm1.rotation = -1 * pendulum1.angle + Math.PI/2;
-  bob1.position = pos1;
-
-  arm2.position.set(pos1.x, pos1.y);
-  arm2.rotation = -1 * pendulum2.angle + Math.PI/2;
-  bob2.position = pos2;
+  if (!paused) {
+	pendulum1.angularVelocity += pendulum1.angularAcceleration * dt / 2;
+	pendulum2.angularVelocity += pendulum2.angularAcceleration * dt / 2;
+  
+	pendulum1.angle += pendulum1.angularVelocity * dt;
+	pendulum2.angle += pendulum2.angularVelocity * dt;
+  
+	pendulum1.updateAngularAcceleration(pendulum1, pendulum2, g);
+	pendulum2.updateAngularAcceleration(pendulum1, pendulum2, g);
+  
+	pendulum1.angularVelocity += pendulum1.angularAcceleration * dt / 2;
+	pendulum2.angularVelocity += pendulum2.angularAcceleration * dt / 2;
+  
+	// Update transformations
+	pos1.x = pendulum1.length * Math.sin(pendulum1.angle);
+	pos1.y = pendulum1.length * Math.cos(pendulum1.angle);
+  
+	pos2.x = pos1.x + pendulum2.length * Math.sin(pendulum2.angle);
+	pos2.y = pos1.y + pendulum2.length * Math.cos(pendulum2.angle);
+  
+	arm1.rotation = -1 * pendulum1.angle + Math.PI/2;
+	bob1.position = pos1;
+  
+	arm2.position.set(pos1.x, pos1.y);
+	arm2.rotation = -1 * pendulum2.angle + Math.PI/2;
+	bob2.position = pos2;
+  }
 
 });
+
+const pauseStartButton = document.getElementById('pause-start-btn') as HTMLButtonElement;
+pauseStartButton.addEventListener('click', () => {
+  paused = !paused;
+  pauseStartButton.textContent = paused ? 'Start' : 'Pause';
+});
+
